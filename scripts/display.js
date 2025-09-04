d34ed26bd6a5129e069040b296e3d4711a905858
bruno <brunodandrademagalhaes@gmail.com>
2025-09-05 00:35:16 +0100

    v1.1 Bug fixes: -Corrected the graph points display to the correct values; -Window Container position; -Removed table and added cards; -Added calculator button

--- scripts/display.js
+++ scripts/display.js
@@ -5,7 +5,7 @@ const resultTextThree = document.getElementById("resultTextThree");
 const resultGraphScale = document.getElementById("resultGraphScale");
 const resultContainer = document.getElementById("resultContainer");
 const graph = document.getElementById("graph");
-const resultTableBody = document.getElementById("resultTableBody");
+const cardsContainer = document.getElementById("cardsContainer");
 
 let percentage = [0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9];
 
@@ -31,8 +31,13 @@ function display() {
   resultGraphScale.innerHTML = `<div class="scaleNumber" id="scaleNumberMax">$${formatter.format(lastNumber)}</div>`;
 
   for (let i = 8; i >= 0; i--) {
-    resultGraphScale.innerHTML += `<div class="scaleNumber">$${formatter.format((lastNumber * percentage[i]).toFixed(2))}</div>`;
+    const calc = (Number(lastNumber) - Number(firstNumber)) * Number(percentage[i]) + Number(firstNumber);
+
+    const value = Number(calc.toFixed(2));
+
+    resultGraphScale.innerHTML += `<div class="scaleNumber">$${formatter.format(value)}</div>`;
   }
+
 
   resultGraphScale.innerHTML += `<div class="scaleNumber" id="scaleNumberMax">$${formatter.format(firstNumber)}</div>`;
   resultContainer.style.display = "flex";
@@ -48,27 +53,28 @@ function display() {
     const midYear = isMiddle
       ? `data-value="Year ${Math.floor(listLength / 2)}"`
       : "";
-    htmlContent += `<div class="graphHeight${midClass}" ${midYear} style="height: calc(${(withCompound[i] * 100) / lastNumber}%)"></div>`;
+
+    htmlContent += `<div class="graphHeight${midClass}" ${midYear} style="height: calc(${((withCompound[i + 1] - firstNumber) / (lastNumber - firstNumber)) * 100}%)"></div>`;
   }
 
   htmlContent += `<div class="graphHeight last" data-value="Year ${listLength}"></div>`;
   graph.innerHTML = htmlContent;
 
   //TABLE
-  resultTableBody.innerHTML = "";
-  resultTableBody.innerHTML = `<tr class="tableHeader">
-                  <td>YEAR</td>
-                  <td>INVESTED</td>
-                  <td>PROFIT</td>
-                </tr>`;
+  cardsContainer.innerHTML = "";
 
   for (let i = 0; i < listLength + 1; i++) {
-    resultTableBody.innerHTML += `<tr>
-  <td>${i}</td>
-  <td>$${formatter.format(withoutCompound[i])}</td>
-  <td>$${formatter.format(withCompound[i] - withoutCompound[i])}</td>
-</tr>`;
+    cardsContainer.innerHTML += `<div class="cardResult">
+
+  <div class="cardResultMoney">
+    <div>Invested $${formatter.format(withoutCompound[i])}</div>
+    <div>Profit of $${formatter.format(withCompound[i] - withoutCompound[i])}</div>
+  </div>
+  <div class="cardResultYear">Year ${[i]}</div>
+</div>`;
   }
 }
+
+
 
 export { display };
